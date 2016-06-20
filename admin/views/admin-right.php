574ba19df82578b3c72b233913c50b30132b8867
ernilambar <nilambar@outlook.com>
2016-06-20 13:28:03 +0545

    Clean up admin right sidebar

--- admin/views/admin-right.php
+++ admin/views/admin-right.php
@@ -1,62 +1,34 @@
 <div class="meta-box-sortables">
-
-    <div class="postbox">
-
-        <h3><span>Help &amp; Support</span></h3>
-        <div class="inside">
-            <h4>Questions, bugs, or great ideas?</h4>
-            <p><a href="http://wordpress.org/support/plugin/ns-category-widget" target="_blank">Visit our plugin support page</a></p>
-        </div> <!-- .inside -->
-
-    </div> <!-- .postbox -->
-
-</div> <!-- .meta-box-sortables -->
-
-<div class="meta-box-sortables">
-
-    <div class="postbox">
-
-        <h3><span>Contribute!</span></h3>
-        <div class="inside">
-            <h4>Wanna help make this plugin better?</h4>
-            <ul>
-                <li><a href="http://wordpress.org/support/view/plugin-reviews/ns-category-widget" target="_blank">Review and rate this plugin on WordPress.org</a></li>
-            </ul>
-        </div> <!-- .inside -->
-
-    </div> <!-- .postbox -->
-
+	<div class="postbox">
+		<h3><span>Help &amp; Support</span></h3>
+		<div class="inside">
+			<ul>
+				<li><strong>Questions, bugs or great ideas?</strong></li>
+				<li><a href="http://wordpress.org/support/plugin/ns-category-widget" target="_blank">Visit our plugin support page</a></li>
+				<li><strong>Wanna help make this plugin better?</strong></li>
+				<li><a href="http://wordpress.org/support/view/plugin-reviews/ns-category-widget" target="_blank">Review and rate this plugin on WordPress.org</a></li>
+			</ul>
+		</div> <!-- .inside -->
+	</div> <!-- .postbox -->
 </div> <!-- .meta-box-sortables -->
 
 <div class="meta-box-sortables">
     <div class="postbox">
-
         <h3><span>My Themes and Plugins</span></h3>
         <div class="inside">
-            <h4>WordPress Theme</h4>
-             <p>
-               <a href="http://wordpress.org/themes/blue-planet" target="_blank">Blue Planet</a>
-             </p>
-            <h4>WordPress Plugins</h4>
-            <p>
-              <a href="http://wordpress.org/plugins/admin-customizer/" target="_blank">Admin Customizer</a>
-            </p>
-            <p>
-              <a href="http://wordpress.org/plugins/really-simple-image-widget/" target="_blank">Really Simple Image Widget</a>
-            </p>
-            <p>
-              <a href="http://wordpress.org/plugins/ns-featured-posts/" target="_blank">NS Featured Posts</a>
-            </p>
-            <p>
-              <a href="http://wordpress.org/plugins/quick-admin/" target="_blank">Quick Admin</a>
-            </p>
-            <p>
-              <a href="http://wordpress.org/plugins/ns-comment-validator/" target="_blank">NS Comment Validator</a>
-            </p>
-            <p>
-              <a href="http://wordpress.org/plugins/date-today-nepali/" target="_blank">Date Today Nepali</a>
-            </p>
+        	<p><strong>WordPress Themes</strong></p>
+        	<ul>
+        		<li><a href="http://wordpress.org/themes/blue-planet" target="_blank">Blue Planet</a></li>
+        		<li><a href="http://wordpress.org/themes/buntu" target="_blank">Buntu</a></li>
+        		<li><a href="http://wordpress.org/themes/simple-life" target="_blank">Simple Life</a></li>
+        	</ul>
+            <p><strong>WordPress Plugins</strong></p>
+            <ul>
+            	<li><a href="http://wordpress.org/plugins/ns-featured-posts/" target="_blank">NS Featured Posts</a></li>
+            	<li><a href="http://wordpress.org/plugins/really-simple-image-widget/" target="_blank">Really Simple Image Widget</a></li>
+            	<li><a href="https://wordpress.org/plugins/woocommerce-product-tabs/" target="_blank">Woocommerce Product Tabs</a></li>
+            	<li><a href="https://wordpress.org/plugins/source-affix/" target="_blank">Source Affix</a></li>
+            </ul>
         </div> <!-- .inside -->
-
     </div> <!-- .postbox -->
 </div> <!-- .meta-box-sortables -->
