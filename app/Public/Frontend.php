2f7ad9b04c8a8679a4ea7875d2c3f038f01c5ee6
Nilambar Sharma <nilambar@outlook.com>
2024-06-20 15:04:29 +0545

    Add minified

--- app/Public/Frontend.php
+++ app/Public/Frontend.php
@@ -31,12 +31,14 @@ class Frontend {
 	 * @since 4.0.0
 	 */
 	public function load_assets() {
+		$min = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
+
 		if ( true === rest_sanitize_boolean( Option::get( 'nscw_field_enable_tree_script' ) ) ) {
-			wp_enqueue_script( 'tree-script', NS_CATEGORY_WIDGET_URL . '/third-party/jstree/js/jstree.js', array( 'jquery' ), '3.3.16', true );
+			wp_enqueue_script( 'tree-script', NS_CATEGORY_WIDGET_URL . '/third-party/jstree/js/jstree' . $min . '.js', array( 'jquery' ), '3.3.16', true );
 		}
 
 		if ( true === rest_sanitize_boolean( Option::get( 'nscw_field_enable_tree_style' ) ) ) {
-			wp_enqueue_style( NS_CATEGORY_WIDGET_SLUG . '-tree-style', NS_CATEGORY_WIDGET_URL . '/third-party/jstree/css/themes/default/style.css', array(), '3.3.16' );
+			wp_enqueue_style( NS_CATEGORY_WIDGET_SLUG . '-tree-style', NS_CATEGORY_WIDGET_URL . '/third-party/jstree/css/themes/default/style' . $min . '.css', array(), '3.3.16' );
 		}
 	}
 }
